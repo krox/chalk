e1cf2057e19c912380064ce5499bb2d7d9d11998
Simon Bürger <simon.buerger@rwth-aachen.de>
2020-05-12 01:15:34 +0200

    implement free group and general monoid ring

---
+++ src/chalk/free_group.h
@@ -0,0 +1,138 @@
+#ifndef CHALK_FREE_GROUP_H
+#define CHALK_FREE_GROUP_H
+
+#include "fmt/format.h"
+#include <stdexcept>
+#include <vector>
+
+namespace chalk {
+
+/** elements of the free group <a,b,...> */
+class FreeProduct
+{
+	std::vector<int> word_; // even = generators, odd = inverse
+
+	/* (...,a,A,...) -> (...) */
+	void cleanup()
+	{
+		size_t j = 0;
+		for (size_t i = 0; i < word_.size(); ++i)
+		{
+			if (j != 0 && word_[i] == (word_[j - 1] ^ 1))
+				--j;
+			else
+				word_[j++] = word_[i];
+		}
+		word_.resize(j);
+	}
+
+  public:
+	FreeProduct() = default;
+	explicit FreeProduct(std::vector<int> word) : word_{std::move(word)}
+	{
+		cleanup();
+	}
+	std::vector<int> const &word() const { return word_; }
+	auto begin() const { return word_.begin(); }
+	auto end() const { return word_.end(); }
+	int operator[](size_t i) const { return word_.at(i); }
+	size_t size() const { return word_.size(); }
+	bool empty() const { return word_.empty(); }
+
+	static FreeProduct generator(int i) { return FreeProduct({i * 2}); }
+
+	bool operator==(FreeProduct const &other) const
+	{
+		return word_ == other.word_;
+	}
+
+	/** only for checking 'x == 1' */
+	bool operator==(int b) const
+	{
+		if (b != 1)
+			throw std::runtime_error("invalid use of 'FreeProduct == int'");
+		return empty();
+	}
+
+	/** arbitrary order */
+	bool operator<(FreeProduct const &other) const
+	{
+		return word_ < other.word_;
+	}
+
+	void operator*=(FreeProduct const &b)
+	{
+		word_.reserve(size() + b.size());
+		for (int a : b)
+			if (!word_.empty() && word_.back() == (a ^ 1))
+				word_.pop_back();
+			else
+				word_.push_back(a);
+	}
+
+	void operator/=(FreeProduct const &b)
+	{
+		word_.reserve(size() + b.size());
+		for (size_t i = 0; i < b.size(); ++i)
+			if (!word_.empty() && word_.back() == b[b.size() - 1 - i])
+				word_.pop_back();
+			else
+				word_.push_back(b[b.size() - 1 - i] ^ 1);
+	}
+
+	FreeProduct operator*(FreeProduct const &b) const
+	{
+		FreeProduct r;
+		r.word_.reserve(size() + b.size());
+		r.word_ = word_;
+		r *= b;
+		return r;
+	}
+
+	FreeProduct operator/(FreeProduct const &b) const
+	{
+		FreeProduct r;
+		r.word_.reserve(size() + b.size());
+		r.word_ = word_;
+		r /= b;
+		return r;
+	}
+};
+
+FreeProduct inverse(FreeProduct const &a)
+{
+	std::vector<int> r;
+	r.resize(a.size());
+	for (size_t i = 0; i < r.size(); ++i)
+		r[i] = a[a.size() - 1 - i] ^ 1;
+	return FreeProduct(std::move(r));
+}
+
+} // namespace chalk
+
+template <> struct fmt::formatter<chalk::FreeProduct>
+{
+	constexpr auto parse(format_parse_context &ctx) { return ctx.begin(); }
+
+	template <typename FormatContext>
+	auto format(const chalk::FreeProduct &prod, FormatContext &ctx)
+	    -> decltype(ctx.out())
+	{
+		// empty product -> '1'
+		if (prod.empty())
+			return format_to(ctx.out(), "1");
+
+		// otherwise print the word (uppercase = inverse)
+		auto it = ctx.out();
+		for (int a : prod)
+		{
+			if (a < 0 || a >= 52)
+				throw std::runtime_error("format(FreeProduct) only supported "
+				                         "for up to 26 generators");
+			it++ = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"[a];
+		}
+		return it;
+	}
+};
+
+#endif
---
+++ src/chalk/group_ring.h
@@ -0,0 +1,274 @@
+#ifndef CHALK_GROUP_RING_H
+#define CHALK_GROUP_RING_H
+
+#include "chalk/free_group.h"
+#include "fmt/format.h"
+#include <vector>
+
+namespace chalk {
+
+template <typename R, typename G> struct MultinomialTerm
+{
+	R coefficient;
+	G index;
+
+	bool operator==(MultinomialTerm const &other) const
+	{
+		return coefficient == other.coefficient && index == other.index;
+	}
+
+	bool operator<(MultinomialTerm const &other) const
+	{
+		return index < other.index;
+	}
+
+	MultinomialTerm operator*(MultinomialTerm const &b) const
+	{
+		return {coefficient * b.coefficient, index * b.index};
+	}
+	void operator*=(MultinomialTerm const &b)
+	{
+		coefficient *= b.coefficient;
+		index *= b.index;
+	}
+};
+
+template <typename R, typename G> class Multinomial
+{
+	std::vector<MultinomialTerm<R, G>> terms_;
+
+	void cleanup()
+	{
+		std::sort(terms_.begin(), terms_.end());
+
+		size_t j = 0;
+		for (size_t i = 0; i < terms_.size(); ++i)
+		{
+			// if index == previous index -> join
+			if (j != 0 && terms_[j - 1].index == terms_[i].index)
+				terms_[j - 1].coefficient += terms_[i].coefficient;
+
+			// else new term
+			else if (i != j)
+				terms_[j++] = std::move(terms_[i]);
+			else
+				j++;
+
+			// if coeff = 0 -> remove term
+			assert(j);
+			if (terms_[j - 1].coefficient == 0)
+				--j;
+		}
+		terms_.resize(j);
+	}
+
+  public:
+	Multinomial() = default;
+	explicit Multinomial(std::vector<MultinomialTerm<R, G>> terms)
+	    : terms_{std::move(terms)}
+	{
+		cleanup();
+	}
+	explicit Multinomial(R const &a) : terms_{MultinomialTerm<R, G>{a, {}}}
+	{
+		cleanup();
+	}
+	explicit Multinomial(int a) : terms_{MultinomialTerm<R, G>{R(a), {}}}
+	{
+		cleanup();
+	}
+
+	static Multinomial generator(int k)
+	{
+		return Multinomial({MultinomialTerm<R, G>{R(1), G::generator(k)}});
+	}
+
+	std::vector<MultinomialTerm<R, G>> const &terms() const { return terms_; }
+
+	void operator+=(Multinomial const &b)
+	{
+		terms_.reserve(terms_.size() + b.terms_.size());
+		for (auto &term : b.terms())
+			terms_.push_back(term);
+		cleanup();
+	}
+	void operator-=(Multinomial const &b)
+	{
+		terms_.reserve(terms_.size() + b.terms_.size());
+		for (auto &term : b.terms())
+			terms_.push_back({-term.coefficient, term.index});
+		cleanup();
+	}
+
+	void operator*=(R const &b)
+	{
+		for (auto &term : terms_)
+			term.coefficient *= b;
+		cleanup();
+	}
+	void operator/=(R const &b)
+	{
+		for (auto &term : terms_)
+			term.coefficient /= b;
+		cleanup();
+	}
+	void operator*=(int b)
+	{
+		for (auto &term : terms_)
+			term.coefficient *= b;
+		cleanup();
+	}
+	void operator/=(int b)
+	{
+		for (auto &term : terms_)
+			term.coefficient /= b;
+		cleanup();
+	}
+};
+
+template <typename R, typename G> bool is_negative(Multinomial<R, G> const &)
+{
+	return false;
+}
+
+template <typename R, typename G>
+bool operator==(Multinomial<R, G> const &a, Multinomial<R, G> const &b)
+{
+	return a.terms() == b.terms();
+}
+
+template <typename R, typename G>
+bool operator==(Multinomial<R, G> const &a, int b)
+{
+	if (a.terms().empty())
+		return b == 0;
+	if (a.terms().size() > 1)
+		return false;
+	return a.terms()[0].index == 1 && a.terms()[0].coefficient == b;
+}
+
+template <typename R, typename G>
+Multinomial<R, G> operator-(Multinomial<R, G> const &a)
+{
+	auto r = a.terms();
+	for (auto &term : r)
+		term.coefficient = -term.coefficient;
+	return Multinomial<R, G>(std::move(r));
+}
+
+template <typename R, typename G>
+Multinomial<R, G> operator+(Multinomial<R, G> const &a,
+                            Multinomial<R, G> const &b)
+{
+	std::vector<MultinomialTerm<R, G>> r;
+	r.reserve(a.terms().size() + b.terms().size());
+	for (auto &term : a.terms())
+		r.push_back(term);
+	for (auto &term : b.terms())
+		r.push_back(term);
+	return Multinomial<R, G>(std::move(r));
+}
+
+template <typename R, typename G>
+Multinomial<R, G> operator-(Multinomial<R, G> const &a,
+                            Multinomial<R, G> const &b)
+{
+	std::vector<MultinomialTerm<R, G>> r;
+	r.reserve(a.terms().size() + b.terms().size());
+	for (auto &term : a.terms())
+		r.push_back(term);
+	for (auto &term : b.terms())
+		r.push_back({-term.coefficient, term.index});
+	return Multinomial<R, G>(std::move(r));
+}
+
+template <typename R, typename G>
+Multinomial<R, G> operator*(Multinomial<R, G> const &a,
+                            Multinomial<R, G> const &b)
+{
+	std::vector<MultinomialTerm<R, G>> r;
+	r.reserve(a.terms().size() * b.terms().size());
+	for (auto &t : a.terms())
+		for (auto &s : b.terms())
+			r.push_back(t * s);
+	return Multinomial<R, G>(std::move(r));
+}
+
+template <typename R, typename G>
+Multinomial<R, G> operator*(Multinomial<R, G> const &a, R const &b)
+{
+	auto r = a.terms();
+	for (auto &term : r)
+		term.coefficient *= b;
+	return Multinomial<R, G>(std::move(r));
+}
+template <typename R, typename G>
+Multinomial<R, G> operator/(Multinomial<R, G> const &a, R const &b)
+{
+	auto r = a.terms();
+	for (auto &term : r)
+		term.coefficient /= b;
+	return Multinomial<R, G>(std::move(r));
+}
+template <typename R, typename G>
+Multinomial<R, G> operator*(R const &a, Multinomial<R, G> const &b)
+{
+	auto r = b.terms();
+	for (auto &term : r)
+		term.coefficient *= a * term.coefficient;
+	return Multinomial<R, G>(std::move(r));
+}
+template <typename R, typename G>
+Multinomial<R, G> operator*(Multinomial<R, G> const &a, int b)
+{
+	auto r = a.terms();
+	for (auto &term : r)
+		term.coefficient *= b;
+	return Multinomial<R, G>(std::move(r));
+}
+template <typename R, typename G>
+Multinomial<R, G> operator/(Multinomial<R, G> const &a, int b)
+{
+	auto r = a.terms();
+	for (auto &term : r)
+		term.coefficient /= b;
+	return Multinomial<R, G>(std::move(r));
+}
+
+template <typename R> using FreeAlgebra = Multinomial<R, FreeProduct>;
+
+} // namespace chalk
+
+template <typename R, typename G>
+struct fmt::formatter<chalk::Multinomial<R, G>>
+{
+	constexpr auto parse(format_parse_context &ctx) { return ctx.begin(); }
+
+	template <typename FormatContext>
+	auto format(const chalk::Multinomial<R, G> &mult, FormatContext &ctx)
+	    -> decltype(ctx.out())
+	{
+		// empty sum -> '0'
+		if (mult.terms().empty())
+			return format_to(ctx.out(), "0");
+
+		// otherwise -> terms with '+' inbetween
+		auto it = ctx.out();
+		for (size_t i = 0; i < mult.terms().size(); ++i)
+		{
+			if (i != 0)
+				it = format_to(it, " + ");
+
+			if (mult.terms()[i].coefficient == 1)
+				it = format_to(it, "{}", mult.terms()[i].index);
+			else if (mult.terms()[i].index == 1)
+				it = format_to(it, "{}", mult.terms()[i].coefficient);
+			else
+				it = format_to(it, "{}*{}", mult.terms()[i].coefficient,
+				               mult.terms()[i].index);
+		}
+		return it;
+	}
+};
+
+#endif
