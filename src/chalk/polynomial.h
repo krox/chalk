03c49b43387e1e2fddeaaabc7fff9df4dc389a63
Simon Bürger <simon.buerger@rwth-aachen.de>
2020-05-12 01:13:36 +0200

    make Polynomial usable as truncated series

--- src/chalk/polynomial.h
+++ src/chalk/polynomial.h
@@ -13,17 +13,26 @@ inline bool is_negative(double x) { return x < 0; }
 
 namespace chalk {
 
+template <typename R> class Polynomial;
+template <typename R>
+Polynomial<R> truncate(Polynomial<R> const &poly, int order);
+
 /**
- * univariate polynomial with dense storage and coefficients in R
+ * Univariate polynomial with dense storage and coefficients in R.
+ * Can also be used truncated power series.
  */
 template <typename R> class Polynomial
 {
 	std::vector<R> coefficients_;
+	int max_order_ = INT_MAX;
 	inline static const R zero_ = R(0); // stupid workaround...
 
 	void cleanup()
 	{
-		while (!coefficients_.empty() && coefficients_.back() == R(0))
+		assert(max_order_ >= 0);
+		while (!coefficients_.empty() &&
+		       ((int)coefficients_.size() - 1 > max_order_ ||
+		        coefficients_.back() == R(0)))
 			coefficients_.pop_back();
 	}
 
@@ -31,8 +40,8 @@ template <typename R> class Polynomial
 	Polynomial() = default;
 	Polynomial(int c) : coefficients_{R(c)} { cleanup(); }
 	Polynomial(const R &c) : coefficients_{c} { cleanup(); }
-	Polynomial(std::vector<R> coefficients)
-	    : coefficients_(std::move(coefficients))
+	Polynomial(std::vector<R> coefficients, int max_order = INT_MAX)
+	    : coefficients_(std::move(coefficients)), max_order_(max_order)
 	{
 		cleanup();
 	}
@@ -40,6 +49,8 @@ template <typename R> class Polynomial
 
 	/** highest power with non-zero coefficient (convention: deg(0) = -1) */
 	int degree() const { return (int)coefficients_.size() - 1; }
+
+	int max_order() const { return max_order_; }
 
 	/** highest is non-zero */
 	std::vector<R> const &coefficients() const { return coefficients_; }
@@ -73,7 +84,47 @@ template <typename R> class Polynomial
 			a /= b;
 		cleanup();
 	}
+	void operator*=(int b)
+	{
+		for (auto &a : coefficients_)
+			a *= b;
+		cleanup();
+	}
+	void operator/=(int b)
+	{
+		for (auto &a : coefficients_)
+			a /= b;
+		cleanup();
+	}
+
+	friend Polynomial<R> truncate<R>(Polynomial<R> const &, int);
 };
+
+template <typename R>
+Polynomial<R> truncate(Polynomial<R> const &poly, int order)
+{
+	assert(order >= 0);
+	if (order >= poly.max_order())
+		return poly;
+	else
+		return Polynomial<R>(poly.coefficients(), order);
+}
+
+template <typename R>
+bool operator==(Polynomial<R> const &a, Polynomial<R> const &b)
+{
+	// assert(a.max_order() == INT_MAX);
+	// assert(b.max_order() == INT_MAX);
+	return a.coefficients == b.coefficients;
+}
+
+template <typename R> bool operator==(Polynomial<R> const &a, int b)
+{
+	// assert(a.max_order() == INT_MAX);
+	if (b == 0)
+		return a.coefficients().empty();
+	return a.coefficients().size() == 1 && a.coefficients()[0] == b;
+}
 
 /** unary operations */
 template <typename R> Polynomial<R> operator-(Polynomial<R> const &a)
@@ -81,7 +132,7 @@ template <typename R> Polynomial<R> operator-(Polynomial<R> const &a)
 	auto coeffs = a.coefficients();
 	for (auto &c : coeffs)
 		c = -c;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 
 /** binary operations (Polynomial <-> Polynomial) */
@@ -92,7 +143,7 @@ Polynomial<R> operator+(Polynomial<R> const &a, Polynomial<R> const &b)
 	c.resize(std::max(a.degree(), b.degree()) + 1);
 	for (int i = 0; i <= b.degree(); ++i)
 		c[i] += b[i];
-	return Polynomial<R>(std::move(c));
+	return Polynomial<R>(std::move(c), std::min(a.max_order(), b.max_order()));
 }
 template <typename R>
 Polynomial<R> operator-(Polynomial<R> const &a, Polynomial<R> const &b)
@@ -101,7 +152,7 @@ Polynomial<R> operator-(Polynomial<R> const &a, Polynomial<R> const &b)
 	c.resize(std::max(a.degree(), b.degree()) + 1);
 	for (int i = 0; i <= b.degree(); ++i)
 		c[i] -= b[i];
-	return Polynomial<R>(std::move(c));
+	return Polynomial<R>(std::move(c), std::min(a.max_order(), b.max_order()));
 }
 template <typename R>
 Polynomial<R> operator*(Polynomial<R> const &a, Polynomial<R> const &b)
@@ -109,8 +160,10 @@ Polynomial<R> operator*(Polynomial<R> const &a, Polynomial<R> const &b)
 	auto coeffs = std::vector<R>(a.degree() + b.degree() + 1);
 	for (int i = 0; i <= a.degree(); ++i)
 		for (int j = 0; j <= b.degree(); ++j)
-			coeffs[i + j] += a[i] * b[j];
-	return Polynomial<R>(std::move(coeffs));
+			if (i + j <= std::min(a.max_order(), b.max_order()))
+				coeffs[i + j] += a[i] * b[j];
+	return Polynomial<R>(std::move(coeffs),
+	                     std::min(a.max_order(), b.max_order()));
 }
 
 /** binary operations (Polynomial <-> R) */
@@ -122,7 +175,7 @@ Polynomial<R> operator+(Polynomial<R> const &a, R const &b)
 		coeffs.resize(1, b);
 	else
 		coeffs[0] += b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 template <typename R>
 Polynomial<R> operator-(Polynomial<R> const &a, R const &b)
@@ -132,7 +185,7 @@ Polynomial<R> operator-(Polynomial<R> const &a, R const &b)
 		coeffs.resize(1, b);
 	else
 		coeffs[0] -= b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 template <typename R>
 Polynomial<R> operator*(Polynomial<R> const &a, R const &b)
@@ -140,7 +193,7 @@ Polynomial<R> operator*(Polynomial<R> const &a, R const &b)
 	auto coeffs = std::vector<R>(a.degree() + 1);
 	for (int i = 0; i <= a.degree(); ++i)
 		coeffs[i] = a[i] * b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 template <typename R>
 Polynomial<R> operator/(Polynomial<R> const &a, R const &b)
@@ -148,7 +201,7 @@ Polynomial<R> operator/(Polynomial<R> const &a, R const &b)
 	auto coeffs = std::vector<R>(a.degree() + 1);
 	for (int i = 0; i <= a.degree(); ++i)
 		coeffs[i] = a[i] / b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 
 /** binary operations (Polynomial <-> int) */
@@ -159,7 +212,7 @@ template <typename R> Polynomial<R> operator+(Polynomial<R> const &a, int b)
 		coeffs.resize(1, R(b));
 	else
 		coeffs[0] += b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 template <typename R> Polynomial<R> operator-(Polynomial<R> const &a, int b)
 {
@@ -168,21 +221,21 @@ template <typename R> Polynomial<R> operator-(Polynomial<R> const &a, int b)
 		coeffs.resize(1, R(b));
 	else
 		coeffs[0] -= b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 template <typename R> Polynomial<R> operator*(Polynomial<R> const &a, int b)
 {
 	auto coeffs = std::vector<R>(a.degree() + 1);
 	for (int i = 0; i <= a.degree(); ++i)
 		coeffs[i] = a[i] * b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 template <typename R> Polynomial<R> operator/(Polynomial<R> const &a, int b)
 {
 	auto coeffs = std::vector<R>(a.degree() + 1);
 	for (int i = 0; i <= a.degree(); ++i)
 		coeffs[i] = a[i] / b;
-	return Polynomial<R>(std::move(coeffs));
+	return Polynomial<R>(std::move(coeffs), a.max_order());
 }
 
 /** op-assigns for convenience (could be optimized...) */
@@ -209,39 +262,79 @@ template <typename R> struct fmt::formatter<chalk::Polynomial<R>>
 	auto format(const chalk::Polynomial<R> &poly, FormatContext &ctx)
 	    -> decltype(ctx.out())
 	{
+		auto it = ctx.out();
+
 		// no terms -> output "0"
 		if (poly.coefficients().empty())
-			return format_to(ctx.out(), "0");
+			it = format_to(it, "0");
 
 		// otherwise list the terms with " + " inbetween
-		auto it = ctx.out();
-		for (int i = poly.degree(); i >= 0; --i)
+		if (poly.max_order() == INT_MAX)
 		{
-			auto c = poly[i];
-			if (c == 0)
-				continue;
-
-			if (is_negative(c))
+			bool first = true;
+			for (int i = poly.degree(); i >= 0; --i)
 			{
-				if (i == poly.degree())
-					it = format_to(it, "-");
-				else
-					it = format_to(it, " - ");
-				c = -c;
+				auto c = poly[i];
+				if (c == 0)
+					continue;
+
+				if (is_negative(c))
+				{
+					if (first)
+						it = format_to(it, "-");
+					else
+						it = format_to(it, " - ");
+					c = -c;
+				}
+				else if (!first)
+					it = format_to(it, " + ");
+				first = false;
+				if (i == 0)
+					it = format_to(it, "{}", c);
+				else if (!(c == 1))
+					it = format_to(it, "{}*", c);
+
+				if (i == 1)
+					it = format_to(it, "x");
+				else if (i >= 2)
+					it = format_to(it, "x^{}", i);
 			}
-			else if (i != poly.degree())
-				it = format_to(it, " + ");
-
-			if (i == 0)
-				it = format_to(it, "{}", c);
-			else if (!(c == 1))
-				it = format_to(it, "{}*", c);
-
-			if (i == 1)
-				it = format_to(it, "x");
-			else if (i >= 2)
-				it = format_to(it, "x^{}", i);
 		}
+		else
+		{
+			bool first = true;
+			for (int i = 0; i <= poly.degree(); ++i)
+			{
+				auto c = poly[i];
+				if (c == 0)
+					continue;
+
+				if (is_negative(c))
+				{
+					if (first)
+						it = format_to(it, "-");
+					else
+						it = format_to(it, " - ");
+					c = -c;
+				}
+				else if (!first)
+					it = format_to(it, " + ");
+				first = false;
+
+				if (i == 0)
+					it = format_to(it, "({})", c);
+				else if (!(c == 1))
+					it = format_to(it, "({})*", c);
+
+				if (i == 1)
+					it = format_to(it, "x");
+				else if (i >= 2)
+					it = format_to(it, "x^{}", i);
+			}
+
+			it = format_to(it, " + O(x^{})", poly.max_order() + 1);
+		}
+
 		return it;
 	}
 };
