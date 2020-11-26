75334f6b27baaf542a169846572c9031ff498a6c
Simon Bürger <simon.buerger@rwth-aachen.de>
2020-11-26 16:34:15 +0100

    tiny additions to 'Floating' type

--- src/chalk/floating.h
+++ src/chalk/floating.h
@@ -96,15 +96,23 @@ template <mpfr_prec_t M> class Floating
 };
 
 /**
- * Typedefs that match the IEEE-754 types very closely. Though actually
- * float/double/__float128_t are probably much faster than
+ * Typedefs that match the IEEE-754 types very closely. For low precision,
+ * float/double/__float128_t are of course much faster than
  * FloatingSingle/Double/Quadruple. MPFR only shines at higher precisions.
+ * IEEE-754 directly only specifies formats up to 'octuple', but it also
+ * defines 'interchange formats' with arbitrary larger widths. These use
+ * 'round(4*log2(W)) - 13' bits for the exponent. From this we derived the
+ * mantissa-width of the large types which therefore correspont to
+ * "16/32/64-fold precision IEEE numbers"
  */
 using FloatingHalf = Floating<11>;
 using FloatingSingle = Floating<24>;
 using FloatingDouble = Floating<53>;
 using FloatingQuadruple = Floating<113>;
 using FloatingOctuple = Floating<237>;
+using FloatingDoubleOctuple = Floating<489>; // "sexdecuple" ?
+using FloatingQuadrupleOctuple = Floating<997>;
+using FloatingOctupleOctuple = Floating<2017>;
 
 /** unary arithmetic */
 template <mpfr_prec_t M> Floating<M> operator+(Floating<M> const &a)
@@ -275,6 +283,11 @@ template <mpfr_prec_t M> bool operator<(Floating<M> const &a, double b)
 		mpfr_##name(r.f_, a.f_, MPFR_RNDN);                                    \
 		return r;                                                              \
 	}
+
+// roots
+CHALK_DEFINE_FLOATING_FUNCTION(sqrt)
+CHALK_DEFINE_FLOATING_FUNCTION(cbrt)
+CHALK_DEFINE_FLOATING_FUNCTION(rec_sqrt)
 
 // logarithms / exponentials
 CHALK_DEFINE_FLOATING_FUNCTION(log)
