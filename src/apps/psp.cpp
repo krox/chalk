8b0c3ca6024ecdce40112040988b1962479a616b
Simon Bürger <simon.buerger@rwth-aachen.de>
2023-01-16 18:01:59 +0100

    redo and rename binary sprp -> psp

--- src/apps/psp.cpp
+++ src/apps/psp.cpp
@@ -10,27 +10,22 @@ using namespace chalk;
 int main(int argc, char *argv[])
 {
 	int64_t base = 2;
-	uint64_t limit = INT64_MAX;
-	bool strong = true;
+	int64_t limit = 1e9;
 
-	CLI::App app{"check probable primes"};
+	CLI::App app{"generate pseudo primes"};
 	app.add_option("--base", base, "base to check");
 	app.add_option("--limit", limit, "only check candidates <= limit");
-	app.add_flag("--strong", strong,
-	             "only report strong pseudo primes (default = true)");
 
 	CLI11_PARSE(app, argc, argv);
 
-	for (uint64_t n = 3; n <= limit; n += 2)
-	{
-
-		if (!strong && !is_prp(base, n))
-			continue;
-		if (strong && !is_sprp(base, n))
-			continue;
-		if (is_prime(n))
-			continue;
-
-		fmt::print("{}\n", n);
-	}
+	auto table = chalk::prime_table(limit, 2);
+	for (int64_t k = 1; k < (int64_t)table[1].size(); ++k)
+		if (!table[1][k])
+		{
+			auto n = 2 * k + 1;
+			if (n > limit)
+				return 0;
+			if (is_prp(base, n))
+				fmt::print("{}\n", n);
+		}
 }
